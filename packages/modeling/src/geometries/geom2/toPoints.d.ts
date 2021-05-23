908f3719cd11bf30d2f0faa3da54fc958164a386
minodisk <daisuke.mino@gmail.com>
2021-05-24 00:58:23 +0900

    Test and fix expansions types

--- packages/modeling/src/geometries/geom2/toPoints.d.ts
+++ packages/modeling/src/geometries/geom2/toPoints.d.ts
@@ -1,7 +1,8 @@
 import Geom2 from './type'
 import Vec2 from '../../maths/vec2/type'
-import { Geometry } from '../types'
+import Path2 from '../path2/type'
 
 export default toPoints
 
 declare function toPoints(geometry: Geom2): Array<Vec2>
+declare function toPoints(geometry: Path2): Array<Vec2>
